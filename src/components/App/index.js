6a6bd659adccce5c3fcc7322607a5738ec4b1f0d
Jon Lee <jdlee022@gmail.com>
2017-09-13 18:11:12 -0700

    updated footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -76,7 +76,7 @@ export default class App extends React.Component {
         </section>
 
         <footer className="row text-center">
-          <p>Version 1.0.0 - 9/10/17</p>
+          <p>Version 1.1.0 - 9/13/17</p>
           <p><a href="https://github.com/jdlee022/Portfolio-React" target="__blank">View source code</a>
           </p>
           <p>Built from scratch with Node and React.</p>
