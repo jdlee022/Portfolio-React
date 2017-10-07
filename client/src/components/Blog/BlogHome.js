40d41d01a0ac46ca34f9729939f2de7215bdbd4c
Jon Lee <jdlee022@gmail.com>
2017-10-07 11:30:19 -0700

    created new database

--- client/src/components/Blog/BlogHome.js
+++ client/src/components/Blog/BlogHome.js
@@ -17,7 +17,17 @@ export default class BlogHome extends React.Component {
     }
 
     render() {
-        var postPreviewItems = this.state.posts.map((post, i) =>
+        //Always sort posts by date createdAt before rendering
+        var sortedPosts = this.state.posts.sort(function(a, b){
+            var keyA = new Date(a.createdAt),
+                keyB = new Date(b.createdAt);
+            // Compare the 2 dates
+            if(keyA > keyB) return -1;
+            if(keyA < keyB) return 1;
+            return 0;
+        });
+
+        var postPreviewItems = sortedPosts.map((post, i) =>
                 <PostPreview data={post} key={i} />
             );
 
